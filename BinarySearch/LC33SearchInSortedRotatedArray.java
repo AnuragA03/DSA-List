881ff42d8201c120e291c6f789008a1f8fc35c22
Anurag <anuragadhikari2003@gmail.com>
2025-04-01 23:22:33 +0530

    LC 33 Search in Sorted Rotated Array updated

--- BinarySearch/LC33SearchInSortedRotatedArray.java
+++ BinarySearch/LC33SearchInSortedRotatedArray.java
@@ -26,36 +26,4 @@ class LC33SearchInSortedRotatedArray {
         }
         return -1; // Target not found
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Input array size
-        System.out.print("Enter the number of elements in the array: ");
-        int n = scanner.nextInt();
-
-        // Input array elements
-        int[] nums = new int[n];
-        System.out.println("Enter the elements of the array (sorted and rotated):");
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-
-        // Input target value
-        System.out.print("Enter the target value to search for: ");
-        int target = scanner.nextInt();
-
-        // Create an instance of LC33SearchInSortedRotatedArray and call the search method
-        LC33SearchInSortedRotatedArray solution = new LC33SearchInSortedRotatedArray();
-        int result = solution.search(nums, target);
-
-        // Output result
-        if (result != -1) {
-            System.out.println("Target found at index: " + result);
-        } else {
-            System.out.println("Target not found in the array.");
-        }
-
-        scanner.close();
-    }
 }
